23ef7cd0eff89cbc076c5e5b68f4f4934536a24d
Mohamad Rahmani <rahmanipour.mohammad@gmail.com>
2023-07-07 22:29:04 +0330

    fixed failing unit test

--- Tests/SumOfTwoV2Tests/SumOfTwoV2Tests.swift
+++ Tests/SumOfTwoV2Tests/SumOfTwoV2Tests.swift
@@ -14,7 +14,7 @@ final class SumOfTwoV2Tests: XCTestCase {
         XCTAssertEqual(sumOfTwo([-2, 1, 3, 4, 6, 7, 8], 2), [-2, 4])
     }
 
-    func test_sumOfTwo_givenArrayWithMoreThan2Items_shouldFail() {
-        XCTAssertEqual(sumOfTwo([-2, 1, 3, 4, 6, 7, 8], 15), [-2, 4])
+    func test_sumOfTwo_givenArrayWithMoreThan2Items_returnsCorrectElements() {
+        XCTAssertEqual(sumOfTwo([-2, 1, 3, 4, 6, 7, 8], 15), [7, 8])
     }
 }
