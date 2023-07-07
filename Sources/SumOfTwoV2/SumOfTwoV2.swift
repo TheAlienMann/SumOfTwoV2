e8a54289feb9773a8040819b07d246484b229e3d
Mohamad Rahmani <rahmanipour.mohammad@gmail.com>
2023-07-07 21:01:29 +0330

    added more test + an almost final solution, using two-pointer

--- Sources/SumOfTwoV2/SumOfTwoV2.swift
+++ Sources/SumOfTwoV2/SumOfTwoV2.swift
@@ -1,4 +1,19 @@
 func sumOfTwo(_ array: [Int], _ target: Int) -> [Int] {
     guard array.count > 1 else { return [] }
-    return []
+    var leftPtr = 0, rightPtr = array.count - 1
+    var diff = Int.max
+    var iIndex = 0, jIndex = 0
+    while rightPtr > leftPtr {
+        if abs(array[leftPtr] + array[rightPtr] - target) < diff {
+            iIndex = leftPtr
+            jIndex = rightPtr
+            diff = abs(array[leftPtr] + array[rightPtr] - target)
+        }
+        if array[leftPtr] + array[rightPtr] > target {
+            rightPtr -= 1
+        } else {
+            leftPtr += 1
+        }
+    }
+    return [array[iIndex], array[jIndex]]
 }
--- Tests/SumOfTwoV2Tests/SumOfTwoV2Tests.swift
+++ Tests/SumOfTwoV2Tests/SumOfTwoV2Tests.swift
@@ -5,4 +5,12 @@ final class SumOfTwoV2Tests: XCTestCase {
     func test_sumOfTwo_givenAnEmptyArray_shouldReturnEmptyArray() {
         XCTAssertEqual(sumOfTwo([], 12), [])
     }
+
+    func test_sumOfTwo_givenArrayWithOneItem_shouldReturnEmptyArray() {
+        XCTAssertEqual(sumOfTwo([1], 12), [])
+    }
+
+    func test_sumOfTwo_givenArrayWith2Items_shouldReturnDesiredArray() {
+        XCTAssertEqual(sumOfTwo([-2, 1, 3, 4, 6, 7, 8], 2), [-2, 4])
+    }
 }
