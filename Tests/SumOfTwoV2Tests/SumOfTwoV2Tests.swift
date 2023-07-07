4716a733f29b9987c228c05ad013ddc26965131a
Mohamad Rahmani <rahmanipour.mohammad@gmail.com>
2023-07-07 21:12:37 +0330

    added another test

--- Tests/SumOfTwoV2Tests/SumOfTwoV2Tests.swift
+++ Tests/SumOfTwoV2Tests/SumOfTwoV2Tests.swift
@@ -13,4 +13,8 @@ final class SumOfTwoV2Tests: XCTestCase {
     func test_sumOfTwo_givenArrayWith2Items_shouldReturnDesiredArray() {
         XCTAssertEqual(sumOfTwo([-2, 1, 3, 4, 6, 7, 8], 2), [-2, 4])
     }
+
+    func test_sumOfTwo_givenArrayWithMoreThan2Items_shouldFail() {
+        XCTAssertEqual(sumOfTwo([-2, 1, 3, 4, 6, 7, 8], 15), [-2, 4])
+    }
 }
