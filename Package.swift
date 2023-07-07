2c5ddbc3a6624f75bab208eba6c849e2e3c51e4a
Mohamad Rahmani <rahmanipour.mohammad@gmail.com>
2023-07-07 13:43:21 +0330

    initial commit

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SumOfTwoV2",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "SumOfTwoV2",
+            targets: ["SumOfTwoV2"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "SumOfTwoV2"),
+        .testTarget(
+            name: "SumOfTwoV2Tests",
+            dependencies: ["SumOfTwoV2"]),
+    ]
+)
